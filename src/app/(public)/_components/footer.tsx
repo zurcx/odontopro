98ffd0834593974421615030146fba270c9ecc81
zurcx <luizfabiodacruz@gmail.com>
2025-08-02 23:54:03 -0300

    Componete Footer

---
+++ src/app/(public)/_components/footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className="py-6 text-center text-gray-500 text-sm md:text-base">
+      <p>Todos direitos reservados &Copy {new Date().getFullYear()} - <span className="hover:text-black duration-300">@sujeitoprogramador</span></p>
+    </footer>
+  )
+}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./_components/footer";
 import { Header } from "./_components/header";
 import { Hero } from "./_components/hero";
 import { Professionals } from "./_components/professionals";
@@ -9,6 +10,7 @@ export default function Home() {
       <div>
         <Hero />
         <Professionals />
+        <Footer />
       </div>
     </div>
   )
