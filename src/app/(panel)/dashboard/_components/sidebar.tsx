1bcb371a96eaa1a145b3fa7571e879f1c73e7c7f
zurcx <luizfabiodacruz@gmail.com>
2025-08-09 07:38:21 -0300

    Finalizado siderbar mobile

--- src/app/(panel)/dashboard/_components/sidebar.tsx
+++ src/app/(panel)/dashboard/_components/sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
+
 import {
   Sheet,
   SheetContent,
@@ -13,6 +14,8 @@ import {
 } from "@/components/ui/sheet"
 import { Button } from '@/components/ui/button';
 import { List } from 'lucide-react';
+import { CalendarCheck2, Folder, Settings, Banknote } from 'lucide-react';
+import Link from 'next/link';
 
 export function SidebarDashboard({ children }: { children: React.ReactNode }) {
 
@@ -28,7 +31,7 @@ export function SidebarDashboard({ children }: { children: React.ReactNode }) {
         "md:ml-64": !isCollapsed
       })}>
 
-        <header className='md:hidden'>
+        <header className='md:hidden flex items-center justify-between border-b px-2 md:px-6 h-14 z-10 sticky top-0 bg-white'>
           <Sheet>
             <div className="flex items-center gap-4">
               <SheetTrigger asChild>
@@ -40,6 +43,51 @@ export function SidebarDashboard({ children }: { children: React.ReactNode }) {
                 Menu OdontPRO
               </h1>
             </div>
+
+            <SheetContent side="right" className="sm:max-w-xs text-black">
+              <SheetTitle>
+                Menu OdontoPRO
+              </SheetTitle>
+              <SheetDescription>
+                Menu Administrativo
+              </SheetDescription>
+
+              <nav className='grid gap-2 text-base pt-5'>
+                <SidebarLink
+                  href="/dashboard"
+                  label="Agendamentos"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<CalendarCheck2 className='w-6 h-6' />}
+                />
+
+
+                <SidebarLink
+                  href="/dashboard/services"
+                  label="Serviços"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<Folder className='w-6 h-6' />}
+                />
+
+                <SidebarLink
+                  href="/dashboard/profile"
+                  label="Meu Perfil"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<Settings className='w-6 h-6' />}
+                />
+
+                <SidebarLink
+                  href="/dashboard/plans"
+                  label="Planos"
+                  pathname={pathname}
+                  isCollapsed={isCollapsed}
+                  icon={<Banknote className='w-6 h-6' />}
+                />
+              </nav>
+
+            </SheetContent>
           </Sheet>
 
         </header>
@@ -53,3 +101,28 @@ export function SidebarDashboard({ children }: { children: React.ReactNode }) {
   )
 }
 
+interface SidebarLinkProps {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+  pathname: string;
+  isCollapsed: boolean;
+}
+
+function SidebarLink({ href, label, icon, pathname, isCollapsed }:
+  SidebarLinkProps) {
+  return (
+    <Link
+      href={href}
+    >
+      <div
+        className={clsx("flex items-center gap-2 px-3 py-2 rounded-md", {
+          "bg-blue-500 text-white": pathname === href,
+          "text-gray-700 hover:bg-gray-100": pathname !== href,
+        })}>
+        <span className='w-6 h-6'>{icon}</span>
+        {!isCollapsed && <span>{label}</span>}
+      </div>
+    </Link >
+  );
+}
