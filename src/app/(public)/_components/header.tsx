6bda944e50172b0de27b9adb35d1cfbac71ea599
zurcx <luizfabiodacruz@gmail.com>
2025-08-11 22:16:12 -0300

    Criando a conta

--- src/app/(public)/_components/header.tsx
+++ src/app/(public)/_components/header.tsx
@@ -12,14 +12,19 @@ import {
 } from "@/components/ui/sheet"
 import { Button } from "@/components/ui/button";
 import { LogIn, Menu } from "lucide-react";
-
+import { useSession } from 'next-auth/react'
+import { handleRegister } from '../_actions/login'
 export function Header() {
+  const { data: session, status } = useSession()
   const [isOpen, setIsOpen] = useState(false);
-  const session = null; // Replace with actual session management logic
+
+
   const navItems = [
     { href: "#profissionais", label: "Profissionais" },
   ]
-
+  async function handleLogin() {
+    await handleRegister("github")
+  }
   const NavLinks = () => (
     <>
 
@@ -35,11 +40,12 @@ export function Header() {
           >{item.label}</Link>
         </Button>
       ))}
-
-      {session ? (
+      {status === 'loading' ? (
+        <></>
+      ) : session ? (
         <Link
           href="/dashboard"
-          className="flex items-center justify-center gap-2"
+          className="flex items-center justify-center gap-2 bg-zinc-900 text-white py-1 rounded-md px-4"
 
         >
           Painel da clínica
@@ -47,7 +53,7 @@ export function Header() {
 
         </Link>
       ) : (
-        <Button>
+        <Button onClick={handleLogin}>
           <LogIn />
           Fazer login
         </Button>
---
+++ src/components/session-auth.tsx
@@ -0,0 +1,11 @@
+import { SessionProvider } from 'next-auth/react'
+
+export function SessionAuthProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      {children}
+    </SessionProvider>
+  )
+
+
+}
